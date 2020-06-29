ef7c3b70f115ab48b7443786c519a178432966d9
danialmalik <danialmalik321@gmail.com>
2020-06-29 09:40:47 +0500

    Add my scripts for branch switching and starting containers

---
+++ myscripts/start-juniper-sync.sh
@@ -0,0 +1,4 @@
+export OPENEDX_RELEASE=juniper.master
+# DEFAULT_SERVICES=lms+studio+discovery+credentials+ecommerce+mysql+mongo+devpi+memcached+firefox+gradebook+elasticsearch
+# DEFAULT_SERVICES=$DEFAULT_SERVICES make dev.sync.up
+make dev.sync.up
